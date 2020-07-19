3681b3439386796eadcd5b95d6ad9eb62288c374
Izuku <63758052+Beki-G@users.noreply.github.com>
2020-07-18 19:00:16 -0700

    initial setup

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const path = require ("path")
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json())
+
+//dummy notes
+const notesArr = [
+    {
+        title:"One",
+        body: "What was I thinking when I signed up for this?"
+    },
+    {
+        title:"Two",
+        body: "Well who knows if this works"
+    },
+];
+
+//Routing user to html 
+app.get("/", (req, res)=>{
+    res.sendFile(path.join(__dirname,"./public/index.html"));
+})
+app.get("/notes", (req, res)=>{
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
+} )
+
+
+//server Listening
+app.listen(PORT, function(){
+    console.log("App listening on PORT "+ PORT)
+})
