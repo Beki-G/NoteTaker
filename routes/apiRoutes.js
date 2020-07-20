5e2abe4bb47b145e1640853246fa8792c9659279
Izuku <63758052+Beki-G@users.noreply.github.com>
2020-07-19 22:08:10 -0700

    add DELETE method and it's functionality. Change how id numbering is calculated.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,22 +1,25 @@
 //Notes array
 const notesArr = require("../db/db");
 
-
 //routing
-
 module.exports= function(app){
 
-    //get raw json file
+    //get current notes, index and return them
     app.get("/api/notes", (req, res)=>{
         //add id to each object
-        notesArr.forEach((note, index)=>{note.id = index;})
+        notesArr.forEach((note, index)=>{note.id = index+1;})
         //send Notes 
         res.send(notesArr)
     })
 
+    
     app.post("/api/notes", (req, res)=>{
-        //post method
-        console.log("I heard there was a post method?")
+        //from the request get object and attach to notesArr
+        notesArr.push(req.body);
+        //Index the new obj
+        (!notesArr) && (notesArr[notesArr.length].id = notesArr.length) 
+
+        res.send(notesArr);
     })
 
     app.delete("/api/notes/:id", (req, res)=>{
@@ -25,7 +28,7 @@ module.exports= function(app){
 
         console.log("Here's the noteID: ", noteId)
 
-        notesArr.splice(noteId, 1);
+        notesArr.splice(noteId-1, 1);
 
         res.send(notesArr)
         
